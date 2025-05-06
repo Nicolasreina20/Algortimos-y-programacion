5445f31c28849a5b12b3e4571d6400e83ce5905e
Nicolasreina20 <nicolastrona11@gmail.com>
2025-05-05 20:28:22 -0500

    Add files via upload

---
+++ Taller Diccionario/ejercicio_2.py
@@ -0,0 +1,6 @@
+datos = {'Mikel': 3, 'Ane': 8, 'Amaia': 12, 'Unai': 5, 'Jon': 8, 'Ainhoa': 7, 'Maite': 5}
+valores_unicos = []
+for valor in datos.values():
+    if valor not in valores_unicos:
+        valores_unicos.append(valor)
+print(valores_unicos)
